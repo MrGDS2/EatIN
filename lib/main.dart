52cb607d7a0103eba285154d7c613884ebbd458a
George Samuels II <mrsonicthunder@gmail.com>
2018-12-18 01:00:15 -0500

    First Flutter Project
    Simple app to help groups decide what to eat that night
    -start up low UI

---
+++ lib/main.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EatIN',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.indigo,
+      ),
+      home: MyHomePage(title: 'What you EatIN?'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+    @override
+     Widget _buildListItem(BuildContext context,DocumentSnapshot snap)
+  {
+    return ListTile(
+      title: Row(children: [
+        Expanded(child: Text(snap['foodName'].toString(),
+        style:Theme.of(context).textTheme.headline,
+        ),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Color(0xffdddddff),
+          ),
+          padding: const EdgeInsets.all(10.0),
+          child: Text(snap['votes'].toString(),
+          style: Theme.of(context).textTheme.display1,
+            
+          ),
+          ),
+      ],
+      ),
+      onTap: (){
+        /*Firestore.instance.runTransaction((transaction)) async {
+         DocumentSnapshot frshsnap =await transaction.get(document.reference);
+         await transaction.update(freshSnap.reference, {
+           'votes': frshSnap['votes'] +1,
+         });  //Better way
+        }*/
+        snap.reference.updateData({
+        'votes' : snap['votes'] +1,
+        });
+      },
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    String _Name="List name";
+
+    var itemTitle= new ListTile(
+      title: new Text("$_Name"),
+    );
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+       body:StreamBuilder(
+        stream: Firestore.instance.collection('Meals').snapshots(),
+        builder: (context,snapshot){
+        //when opens data will not be there
+        if(!snapshot.hasData) return const Text('Please Wait...');
+         return ListView.builder(
+         itemExtent: 80.0,
+         itemCount: snapshot.data.documents.length,
+         itemBuilder: (context, index)=>
+         _buildListItem(context,snapshot.data.documents[index]),
+                  );   
+                 }
+                 
+                 // ListView.builder(
+                 //   itemExtent:80.0,
+                 //   itemCount:
+                 //   itemBuilder: (context,index)=>
+         
+         
+                 
+               )
+               );
+           }
+         }
+         
+
+
+
+
+
+class FireBaseRef
+{
+  // static Future<StreamFood<Event>> getFoodStream(String key,
+  // void onData(Todo todo) ) async {
+
+  // String FoodName= await Preferences.getName();
+  // StreamFood<Event> food = FirebaseDatabase.instance
+  // .reference()
+  // .child("Category")
+  // .child()
+  // .child()
+  // .child(FoodName)
+  // .onValue
+  // .listen((Event event)){
+  //   var shot= new Todo.fromJson(event.snapshot.key,event.snapshot.value)
+  // }
+
+
+
+  // }
+  
+  
+  // )
+}
+
+
+
